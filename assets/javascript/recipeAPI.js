215bd3cc33715158f01c04707fa37904e0c7ed80
dbhujle <bhujle.Deepa@gmail.com>
2019-05-10 12:38:13 -0500

    update #2

--- assets/javascript/recipeAPI.js
+++ assets/javascript/recipeAPI.js
@@ -1,1 +1,16 @@
 //code goes herre to search recipes from SPoonacular
+let searchTerm = 'indian';
+
+const result = {
+  "async": true,
+  "crossDomain": true,
+  "url": `https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/searchComplex?query=burger&cuisine=american`,
+  "method": "GET",
+  "headers": {
+    "X-RapidAPI-Key": "d8801f3ce8msha12100d587bc143p151da1jsn3c1a652ff0a6"
+  }
+}
+
+$.ajax(settings).done(response => {
+  console.log(response);
+});
